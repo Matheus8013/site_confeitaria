bc740753e40b40337902a73f0a777678402bfb2e
Matheus Ferreira <matheusfeereirabispo1380@gmail.com>
2023-11-18 08:32:30 -0300

    commt inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$('document').ready(function() {
+    $('#telefone').mask('(00)00000-0000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: '* Por favor, insira seu nome completo',
+            email: '* Por favor, insira seu email',
+            mensagem: '* Por favor, insira uma mensagem'
+        }
+    })
+})
